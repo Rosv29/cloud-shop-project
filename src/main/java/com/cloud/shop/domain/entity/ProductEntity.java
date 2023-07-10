0bb5e4724e75088fcfa09afd520fd86a0420c388
rosv <rosv@220.116.241.119>
2023-07-10 23:38:33 +0900

    1138

--- src/main/java/com/cloud/shop/domain/entity/ProductEntity.java
+++ src/main/java/com/cloud/shop/domain/entity/ProductEntity.java
@@ -1,6 +1,10 @@
 package com.cloud.shop.domain.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,15 +15,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-//@Entity
+@Entity
 public class ProductEntity {
 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private long no;
 	
+	@Column(nullable = false)
 	private String name;
 	
+	@Column(nullable = false)
 	private String price;
 	
+	@Column(nullable = false)
 	private String content;
 	
 	
